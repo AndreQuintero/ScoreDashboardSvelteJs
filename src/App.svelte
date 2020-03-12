5318b35276b8c3321e6b44363d2417823583e64f
Andre Quintero dos Santos <andre.santos@spcbr.int.br>
2020-03-12 18:17:54 -0300

    separando a lógica do app e colocando rotas na aplicacao

--- src/App.svelte
+++ src/App.svelte
@@ -1,42 +1,24 @@
 <script>
 	import Navbar from './Navbar.svelte';
 	import Player from './Player.svelte';
-	import AddPlayer from './AddPlayer.svelte';
+	import Scoreboard from './Scoreboard.svelte';
+	import { Router, Link, Route } from 'svelte-routing';
+	import NewPage from './NewPage.svelte';  
 
-	let players = [
-		{
-			name: "André",
-			points: 100
-		},
-		{
-			name: "Diego",
-			points: 100
-		}
-	];
-
-	const addPlayer = (e) => {
-		const newPlayer = e.detail;
-		players = [...players, newPlayer];
-
-	};
-
-	const removePlayer = (e) => {
-		players = players.filter(player => player.name !== e.detail);
-	}
+	export let url = "";
 
 </script>
+<Router url="{url}">
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+  </nav>
+  <div>
+    <Route path="about" component="{NewPage}" />
+    <Route path="/" component="{Scoreboard}"></Route>
+  </div>
+</Router>
 
-<Navbar />
-<div class="container">
-	{#if players.length === 0}
-		<p>No players</p>
-	{:else} 
-		{#each players as player}
-			<Player name={player.name} points={player.points} on:removePlayer={removePlayer}/>
-		{/each}	
-	{/if}
-	<AddPlayer on:addPlayer={addPlayer}/>
-</div>
 
 <style>
 </style>
---
+++ src/Scoreboard.svelte
@@ -0,0 +1,40 @@
+<script>
+	import Navbar from './Navbar.svelte';
+	import Player from './Player.svelte';
+	import AddPlayer from './AddPlayer.svelte';
+	let players = [
+		{
+			name: "André",
+			points: 100
+		},
+		{
+			name: "Diego",
+			points: 100
+		}
+	];
+
+	const addPlayer = (e) => {
+		const newPlayer = e.detail;
+		players = [...players, newPlayer];
+
+	};
+
+	const removePlayer = (e) => {
+		players = players.filter(player => player.name !== e.detail);
+	}
+
+</script>
+<Navbar />
+<div class="container">
+	{#if players.length === 0}
+		<p>No players</p>
+	{:else} 
+		{#each players as player}
+			<Player name={player.name} points={player.points} on:removePlayer={removePlayer}/>
+		{/each}	
+	{/if}
+	<AddPlayer on:addPlayer={addPlayer}/>
+</div>
+
+<style>
+</style>
