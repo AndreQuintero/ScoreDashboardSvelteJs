5cd4d0704357947ab2bbcb300496669aae3bbfb6
Andre Quintero dos Santos <andre.santos@spcbr.int.br>
2020-03-12 16:14:23 -0300

    svelte js dashboard

---
+++ src/AddPlayer.svelte
@@ -0,0 +1,26 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+    
+    let player = {
+        name: "",
+        points: 0
+    };
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        dispatch("addPlayer", player);
+        player = {
+        name: "",
+        points: 0
+    };
+    }
+</script>
+
+<form on:submit={onSubmit} class="grid-3">
+    <input type="text" placeholder="Player Name" bind:value={player.name}>
+    <input type="number" placeholder="Player Points" bind:value={player.points}>
+    <button type="submit" class="btn btn-primary">Add player</button>
+</form>
